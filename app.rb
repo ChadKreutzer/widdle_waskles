b7e0db2c73600e10bfe0756628245489228d2018
ChadKreutzer <kanthalion@gmail.com>
2017-02-28 15:44:38 -0700

    rough in photos view

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class MyApp < Sinatra::Base
   # compress assets
   environment.js_compressor  = :uglify
   environment.css_compressor = :scss
-  
+
   # get assets
   get "/assets/*" do
     env["PATH_INFO"].sub!("/assets", "")
@@ -22,40 +22,41 @@ class MyApp < Sinatra::Base
 
   get '/' do
     today = Date.today
+    @photos = Photo.all
     @testimonial = Testimonial.order("RANDOM()").first
     @specials = Special.where("expires >= ?", today)
     erb :index
   end
-  
+
   # Testimonials Database CRUD
   post '/testimonials/new' do
     @testimonial = Testimonial.create(story: params[:story], name: params[:name])
     redirect '/testimonials'
   end
-  
+
   get '/testimonials' do
     @testimonials = Testimonial.all
     erb :'testimonials/index', :layout => false
   end
-  
+
   get "/testimonials/:id" do
     @testimonial = Testimonial.find(params[:id])
     erb :'testimonials/post_page', :layout => false
   end
-  
+
   put '/testimonials/:id' do
     @testimonial = Testimonial.find(params[:id])
     @testimonial.update(story: params[:story], name: params[:name])
     @testimonial.save
     redirect '/testimonials/' + params[:id]
   end
-  
+
   delete '/testimonials/:id/delete' do
     @testimonial = Testimonial.find(params[:id])
     @testimonial.destroy
     redirect '/testimonials'
   end
-  
+
   # Specials Database CRUD
   post '/specials/new' do
     @special = Special.create(headline: params[:headline],
@@ -64,17 +65,17 @@ class MyApp < Sinatra::Base
                               )
     redirect '/specials'
   end
-  
+
   get '/specials' do
     @specials = Special.all
     erb :'specials/index', :layout => false
   end
-  
+
   get "/specials/:id" do
     @special = Special.find(params[:id])
     erb :'specials/post_page', :layout => false
   end
-  
+
   put '/specials/:id' do
     @special = Special.find(params[:id])
     @special.update(headline: params[:headline],
@@ -84,13 +85,13 @@ class MyApp < Sinatra::Base
     @special.save
     redirect '/specials/' + params[:id]
   end
-  
+
   delete '/specials/:id/delete' do
     @special = Special.find(params[:id])
     @special.destroy
     redirect '/specials'
   end
-  
+
   # Photos Database CRUD
   post '/photos/new' do
     @photo = Photo.create(before_filename: params[:before_filename][:filename],
@@ -99,20 +100,20 @@ class MyApp < Sinatra::Base
                           )
     file_action(:before_filename)
     file_action(:after_filename)
-    
+
     redirect '/photos'
   end
-  
+
   get '/photos' do
     @photos = Photo.all
     erb :'photos/index', :layout => false
   end
-  
+
   get "/photos/:id" do
     @photo = Photo.find(params[:id])
     erb :'photos/post_page', :layout => false
   end
-  
+
   put '/photos/:id' do
     @photo = Photo.find(params[:id])
     unless params[:before_filename].blank?
@@ -125,13 +126,13 @@ class MyApp < Sinatra::Base
       @photo.update(after_filename: params[:after_filename][:filename])
       file_action(:after_filename)
     end
-    
+
     @photo.update(caption: params[:caption])
     @photo.save
-    
+
     redirect '/photos/' + params[:id]
   end
-  
+
   delete '/photos/:id/delete' do
     @photo = Photo.find(params[:id])
     file_action(@photo.before_filename)
@@ -139,7 +140,7 @@ class MyApp < Sinatra::Base
     @photo.destroy
     redirect '/photos'
   end
-  
+
   def file_action(name)
     if name.is_a? Symbol
       @filename = params[name][:filename]
